87108da6b299a0d198412a4f9a2aa4a3e849e124
Elturan_Sultankulov <elturansultankulov1@gmail.com>
2024-12-11 20:21:30 +0600

    Добавил метод initStateObj, геттеры, сеттеры. Подкорректировал метод changeDriver

--- Task2/src/models/Truck.java
+++ Task2/src/models/Truck.java
@@ -1,7 +1,12 @@
 package models;
 
 import exceptions.StateException;
+import states.OnBase;
+import states.OnRepair;
+import states.OnRoute;
 import states.State;
+
+import java.util.Objects;
 
 public class Truck {
 
@@ -9,8 +14,25 @@ public class Truck {
     private String name;
     private String driver;
     private String state;
-    private State stateObj;
-    private Driver driverObj;
+    private transient State stateObj;
+
+    private void initStateObj() {
+        switch (state) {
+            case "On base":
+                stateObj = new OnBase();
+                break;
+            case "On route":
+                stateObj = new OnRoute();
+                break;
+            case "On repair":
+                stateObj = new OnRepair();
+                break;
+            default:
+                stateObj = new OnBase();
+                state = "On base";
+                break;
+        }
+    }
 
     public int getId() {
         return id;
@@ -28,9 +50,23 @@ public class Truck {
         return stateObj;
     }
 
+    public void setDriver(String driver) {
+        this.driver = driver;
+    }
+
+    //    public String toString() {
+//        String l = "%3s | %-17s | %10s | %-8s";
+//        return  "| " + " " + name + " " + " " + driver + state;
+//    }
+
     public String toString() {
-        String l = "%3s | %-17s | %10s | %-8s";
-        return  "| " + " " + name + " " + " " + driver + state;
+        final StringBuilder sb = new StringBuilder("Truck{");
+        sb.append("id").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", driver='").append(driver).append('\'');
+        sb.append(", state='").append(state).append('\'');
+        sb.append('}');
+        return sb.toString();
     }
 
     public void setState(String state) {
@@ -41,13 +77,13 @@ public class Truck {
         this.stateObj = stateObj;
     }
 
-    public void setDriverObj(Driver driverObj) {
-        this.driverObj = driverObj;
-    }
+//    public void setDriverObj(Driver driverObj) {
+//        this.driverObj = driverObj;
+//    }
 
-    public Driver getDriverObj() {
-        return driverObj;
-    }
+//    public Driver getDriverObj() {
+//        return driverObj;
+//    }
 
     public void startDriving() {
         try {
@@ -68,6 +104,8 @@ public class Truck {
     }
 
     public void changeDriver() {
+        if (Objects.isNull(stateObj))
+            initStateObj();
         try {
             stateObj.changeDriver(this);
         } catch (StateException se) {
